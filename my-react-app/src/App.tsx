bd59e5c291f7e6e1b636d3c4364cf8251d753502
Roi Susi <roisusi@gmail.com>
2025-02-21 13:12:05 +0200

    first init

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Terminal from "./components/terminal/Terminal.tsx";
+
+function App() {
+    return (
+        <>
+            <Terminal/>
+        </>
+    )
+}
+
+export default App
